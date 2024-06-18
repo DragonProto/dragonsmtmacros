8307fd3345519b9d9d5fa092ecf0f93f1c29dcd1
Alexander <alexanderhoppett@gmail.com>
2024-06-18 14:41:11 +0200

    Tons of CSS changes and changes to categories

--- exportToJson.js
+++ exportToJson.js
@@ -1,18 +1,47 @@
 const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs');
+const path = require('path');
 
-const db = new sqlite3.Database('./dnd.db');
+// Path to your database
+const dbPath = path.join(__dirname, 'dnd.db');
+// Path to your JSON file
+const jsonPath = path.join(__dirname, 'macros.json');
 
-db.serialize(() => {
-  db.all('SELECT * FROM macros', [], (err, rows) => {
-    if (err) {
-      throw err;
-    }
+function exportToJson() {
+  const db = new sqlite3.Database(dbPath);
 
-    const jsonData = JSON.stringify(rows, null, 2);
-    fs.writeFileSync('macros.json', jsonData);
-    console.log('JSON data has been saved to macros.json');
+  db.serialize(() => {
+    db.all(`
+      SELECT macros.*, 
+             CASE 
+               WHEN spell_subcategories.subcategory_id IS NOT NULL THEN GROUP_CONCAT(subcategories.name, ', ')
+               ELSE macros.subcategory
+             END AS subcategories
+      FROM macros
+      LEFT JOIN spell_subcategories ON macros.id = spell_subcategories.spell_id
+      LEFT JOIN subcategories ON subcategories.id = spell_subcategories.subcategory_id
+      GROUP BY macros.id
+      ORDER BY macros.name ASC
+    `, [], (err, rows) => {
+      if (err) {
+        console.error('Error fetching data: ', err);
+        return;
+      }
+
+      // Sort subcategories alphabetically
+      rows.forEach(row => {
+        if (row.subcategories) {
+          row.subcategories = row.subcategories.split(', ').sort().join(', ');
+        }
+      });
+
+      const jsonData = JSON.stringify(rows, null, 2);
+      fs.writeFileSync(jsonPath, jsonData);
+      console.log('JSON data has been saved to macros.json');
+    });
   });
-});
 
-db.close();
+  db.close();
+}
+
+exportToJson();
